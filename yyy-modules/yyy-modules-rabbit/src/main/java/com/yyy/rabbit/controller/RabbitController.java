2e70e9bed4e38a8ec843d8cfe9ea744e96141655
yyy <yyy@gmail.com>
2024-07-04 11:42:14 +0800

    修改rabbitmq服务无用注释代码

--- yyy-modules/yyy-modules-rabbit/src/main/java/com/yyy/rabbit/controller/RabbitController.java
+++ yyy-modules/yyy-modules-rabbit/src/main/java/com/yyy/rabbit/controller/RabbitController.java
@@ -5,6 +5,7 @@ import com.yyy.common.core.domain.R;
 import com.yyy.rabbit.IMQSender;
 import com.yyy.rabbit.enums.DelayTypeEnum;
 import com.yyy.rabbit.model.PayOrderMchNotifyMQ;
+import com.yyy.rabbit.model.ResetAppConfigMQ;
 import com.yyy.rabbit.model.SkillOrderMQ;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -27,18 +28,18 @@ public class RabbitController {
 
     @GetMapping("/sendtestMQ2")
     public void sendtestMQ2() {
-        mqSender.send(PayOrderMchNotifyMQ.build(20l));
-//        mqSender.send(PayOrderMchNotifyMQ.build(20l),10);
-        //
-        //mqSender.send(ResetIsvMchAppInfoConfigMQ.build("5", "2", "6777", "888"));
-        //注意广播方式的，延时不会生效
-//        mqSender.send(ResetAppConfigMQ.build("abccccc"),10);
+        mqSender.send(PayOrderMchNotifyMQ.build(20l));  //测试点对点模式 有异步注解时的效果
+//        mqSender.send(PayOrderMchNotifyMQ.build(20l),10);  //测试点对点模式 有异步注解且延时时的效果
+
+
+//        mqSender.send(ResetAppConfigMQ.build("abccccc"),10); //测试广播模式，注意广播方式的，延时不会生效
 
     }
 
     @GetMapping("/sendSkillOrderMQ/{orderId}")
     public R sendSkillOrderMQ(@PathVariable("orderId")Long orderId) {
-        mqSender.send(SkillOrderMQ.build(orderId));
+        mqSender.send(SkillOrderMQ.build(orderId));  //测试点对点模式 没有有异步注解时的效果
+//        mqSender.send(SkillOrderMQ.build(orderId),10);  //测试点对点模式 没有有异步注解且延时时的效果
         return R.ok();
     }
 
